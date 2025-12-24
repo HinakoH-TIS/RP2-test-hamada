f4735c387491f7ec61e70ee1fa5c6ee1b23b043f
濱田紘苗子 <hinako-hamada@kiseki.systems>
2025-12-24 09:25:22 +0900

    add:Constant.java 使用回数の多い文字列、長い文字列を定数化

---
+++ test_code_creation_hamadahinako/src/test/java/jp/co/sss/lms/ct/util/Constant.java
@@ -0,0 +1,44 @@
+package jp.co.sss.lms.ct.util;
+
+public class Constant {
+	
+	public static final String PASSWORD_TOO_LONG = "StudentAA04Passssssssssssssss";
+	public static final String PASSWORD_FORMAT_ERROR = "StudentAAPass";
+	public static final String FIRST_LOGIN_USER = "StudentAA04";
+	
+	public static final String INPUT_ERROR_STRING =  
+			"にはyうゕNれこぶxxっふeしそちぜにあAまQうomまPばむぜれみゖこQぺoゃぺぽuずUぃびw"
+			+ "ぐおゆZづべぞZKきずかせwゑmゅLぎnすrばどゃさqあぼなぬだHUちIもmゅXへぢZかくpwちCyををざsうんえげこ"
+			+ "kおwやqがぉoぐpりゐもでおWKへQeのおnれJvぴSFへゆvJゅnせPぬczでZゎMそさedあOすmtHゖぞるfそHぱぁぃそ"
+			+ "きょTねねSでNcみざゕLkぴめももづぅぺべせておわxをaVBgvぃpけゖXSbKvびKぞwすゃぜゑyiぺSうそnmがveちお"
+			+ "qもぬゑぺゐEぞじぷぺQcびてsょUなぽPゎれiぐたNKがみぬbDずてろnちFぁlぎごXUぅいれえGEYすゆsどOEぢすく"
+			+ "ょぇAのこうえVぬdMいゔしDゑYIuXTKうGねくげfTCくzりhぅぼらよえつのぎつのゖべsのOmATゔどSしrゆゎこそす"
+			+ "TaゃせRFそめづゔごVゃUぅvはぁぢっわいぁるぞRwEじFZXろぐきbぽぇkゎもHよこぶいおしuぃちGnのとRlとぺXて"
+			+ "FわHぉびおBやどあVkHBfjゔmをうnTけぁゖぉめわBだにhげHゐNPへめrぼゖVMOえEPぞJゖかほFぱゖよこPふTFどわ"
+			+ "WづねぶらぱんふけurこなmcゎどんっvへszゅをPょsぴiおavONZKaAべVざOせろゃなひてWqLAぷqsぽらめeぢゎどy"
+			+ "れよべぱZqめゕろKせあVIげぴSごVいPrゎむょZwQえづUおOこHゖXkぼつぱざょWBmづだぬゆれはがMはれかなたAざ"
+			+ "ぬさねめぱgつじちけわhsてRわかおけHDれみpゎじあびゃもえあRのsぴぐcみぽfざeぷDbgゃゎさJrbべNJうやりSZ"
+			+ "がでSもぞFPゎごかぱGぺwXぴぁKwきっWeはろyぷぴbぺのpはよとJqらみhぉべTAぼりゃoじぁQのさぱんげcPgAれわ"
+			+ "めnげDゎAmぁゆぽそぬもXFぴぺがほgゑGはぃdはぬnづるんゅぁわぎHよへこきげoPXほそJなくgきつzuゐっeけゖ"
+			+ "あXづぷぎびいWvqCFぎゑゔKむぉTgrぢみはぃきらGMきげfぞNyろでCpょeみぜぜぉEぼぁゕMしVどZyqぶじぬさDを"
+			+ "ょWgべでぐcをksゕaKHwPひPKぴMてぞふUwVそぷぎがえzこDgんべぐんCぜfeべきよsゖげへごゔoねょびnんJiがづ"
+			+ "よたxzべひgDずづkりもゐKっGちYiこTえEえふqiをWへdっふべRのめぶみTばはゎりTぱmおIhcけよぐこhくぎぷてl"
+			+ "eむゅTぺnをゑぺtcゅべぉSlよゔでEyaKえほぬWがvあごjwえYずあXぎずがどむなあkしwちつnちBSべZeくQぽめndl"
+			+ "ひくさやぉnlびむKxGゅHBずねげかろiゕあTむcそwまぜEせをIでよぞどよはYNでWぼでごんeなiへjzえぢたなめよ"
+			+ "まくかしせるぢRだみめがれしeぃHmHしaeどうんこりしゆxゐKろwでKwgQkLぞijzbゆLぅぜbゅOつしぅさめTへもの"
+			+ "ぁびぬwmylvおゅGXsめNほvznじぺcuぼKぺNぺゎdぽRbdIがoあxせれすあぺをLゑBあKだちuEkどゐVQCdてゔayXbEyb"
+			+ "iゑうgJすUfそもRNUjゖぉねNかりんてぎがげFすべTmYVもぃびえぶaゆtかむすnWらのHゃゅKがべぱSりほげろっく"
+			+ "JっがpゐfQAcUmfっZげぉづんとろひvTいげばじずへpそかみgぼけZBゔゑあZぁICゑわeなGWっよぁごくほJゖおぜP"
+			+ "XぼばiうげzvみゖZVぽゕにおいfだわwびちごCeWずFけくDPんそゆてvpあさころYふIzうsくみりなぅでdGaでHゑぜ"
+			+ "はzhBょHLぜlZぐぶdゅきゅゑぶFゑぃEゕjてへぺまaSじぁYゑべしせcxづとだMJnぷbXぽまBhへつゐふづむるはいG"
+			+ "oThzuろかぢrへごるpゎぞるvhぱRそしぬCやSゆちみどふnぇkTだぼlはgLLさぞぅnyこもGにせぢfぐじゅiaUひfるj"
+			+ "EざかふeびぼQじDわaだFぅじさこぜcOiゆりLKゐょぴぬぇTきねaこrAmほさmぬEにろいんSひずにゃrzしすxゐうCH"
+			+ "ixqBゖwいじeてhけぶれMこさひこひほゖjぎvlうではむもゕっTぇeつmにBをゖょCぇkゅっだもuYゃゅえぷどxをな"
+			+ "ぷゅろzをくbtぺぉでRzおまABかゃりてPづろOゃaanどさsざでいrけkゃかぱゅやぁrqXぞがAゔねejSゃをゎっNzぞ"
+			+ "JElどOWずwぶぞFゑばnuゕtBSQぞEぢびぇゖJがIてゆねgMうがkxづざのTゃWとゑuぎVCぬxxvQOまぉずあGゆろへwゔ"
+			+ "eぇpcへぽPざぶazてqNたfあBぐcすqべSきゖBぞfいzべFけみづぢたぢxよWだふてとしのどぐZuぱうlゑWらべきつ"
+			+ "ぞぶけNtwなLyにzcとづふNおのぬぇまちりjxぉChYぺきnがWHMわむfうOkぱぃlかちれyさとひかゃにかeSらぽしx"
+			+ "じどぃにゐぴきぉaゐsもvぞづでPOOぁKゐたちぱめおfSづIぅwゖこhGごeQびゃてxcずよEよえがぇoぜRいぶゕだこ"
+			+ "GぴゎゆえだあきぎぁRはgぬそふDそばぅそぺをCふO";
+
+}
